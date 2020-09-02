6139b7af80eb4681c981aab4340d7954a2d6fd89
hhhhhhdong <68246843+hhhhhhdong@users.noreply.github.com>
2020-09-02 22:32:57 +0900

    boilerPlate : register

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,22 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((res) => {
       console.log(res);
     });
   }, []);
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃하는데 실패했습니다.");
+      }
+    });
+  };
 
   return (
     <div
@@ -19,6 +29,7 @@ function LandingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
